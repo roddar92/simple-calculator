ccad164d350e50c7662f85ad23c8f18a3ff402ce
Dashka Rodionova <rodionowadarja@yandex.ru>
2016-06-05 18:08:15 +0300

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,31 @@
+class Operation(object):
+
+    def __init__(self):
+        self.name = ""
+
+class AdditionOperation(Operation):
+
+    def __init__(self, data):
+        self.name = "plus"
+        self.data = data
+    
+    def eval(a, b)
+        return data[0] + data[2]
+        
+class SubtractOperation(Operation):
+
+    def __init__(self, data):
+        self.name = "minus"
+        self.data = data
+    
+    def eval(a, b)
+        return data[0] - data[2]
+        
+class MultiplicateOperation(Operation):
+
+    def __init__(self, data):
+        self.name = "mult"
+        self.data = data
+    
+    def eval()
+        return data[0] * data[2]
