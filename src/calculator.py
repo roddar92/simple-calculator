f74d956189321e71012415dc339c2e2861516512
roddar92 <rodionowadarja@yandex.ru>
2016-06-11 00:02:23 +0300

    Choose operation from the dictionary

--- src/calculator.py
+++ src/calculator.py
@@ -6,7 +6,6 @@ import operations as ops
 
 
 class Calculator(object):
-
     def __init__(self):
         """Create dictionary with arithmetic operations"""
         self.operations = dict()
@@ -33,66 +32,46 @@ class Calculator(object):
         self.operations["acos"] = ops.AcosOperation()
         self.operations["atan"] = ops.AtanOperation()
 
-
-
     def operation(self, data, unary=False):
+        """Evaluate input data by unary property"""
         try:
             if unary:
-                data[1] = m.e if data[1].lower() == "e" else float(data[1])
-                data[1] = m.pi if data[1].lower() == "pi" else float(data[1])
+                data[1] = data[1].lower()
+                if data[1] == "e":
+                    data[1] = m.e
+                elif data[1] == "pi":
+                    data[1] = m.pi
+                else:
+                    data[1] = float(data[1])
             else:
-                data[0] = m.e if data[0].lower() == "e" else float(data[0])
-                data[0] = m.pi if data[0].lower() == "pi" else float(data[0])
-                data[2] = m.e if data[2].lower() == "e" else float(data[2])
-                data[2] = m.pi if data[2].lower() == "pi" else float(data[2])
+                data[0] = data[0].lower()
+                data[2] = data[2].lower()
+                if data[0] == "e":
+                    data[0] = m.e
+                elif data[0] == "pi":
+                    data[0] = m.pi
+                else:
+                    data[0] = float(data[0])
+
+                if data[2] == "e":
+                    data[0] = m.e
+                elif data[2] == "pi":
+                    data[2] = m.pi
+                else:
+                    data[2] = float(data[2])
         except ValueError:
             return "Input operands is not numbers"
 
-        result = 0.0
-        if unary:
-            if data[0] == "inv":
-                result = 1 / data[1]
-            elif data[0] == "sqrt":
-                result = m.sqrt(data[1])
-            elif data[0] == "log":
-                result = m.log2(data[1])
-            elif data[0] == "lg":
-                result = m.log10(data[1])
-            elif data[0] == "ln":
-                result = m.log(data[1], m.e)
-            elif data[0] == "exp":
-                result = m.exp(data[1])
-            elif data[0] == "sin":
-                result = m.sin(data[1])
-            elif data[0] == "cos":
-                result = m.cos(data[1])
-            elif data[0] == "tan":
-                result = m.tan(data[1])
-            elif data[0] == "ctan":
-                result = 1 / (m.tan(data[1]))
-            elif data[0] == "asin":
-                result = m.asin(data[1])
-            elif data[0] == "acos":
-                result = m.acos(data[1])
-            elif data[0] == "atan":
-                result = m.atan(data[1])
+        operation = data[0] if unary else data[1]
+        if operation in self.operations:
+            op = self.operations[operation]
+            if op.unary:
+                result = op.eval_unary(data[1])
+            else:
+                result = op.eval_binary(data[0], data[2])
         else:
-            if data[1] == "plus":
-                result = data[0] + data[2]
-            elif data[1] == "minus":
-                result = data[0] - data[2]
-            elif data[1] == "mult":
-                result = data[0] * data[2]
-            elif data[1] == "divide":
-                result = data[0] / data[2]
-            elif data[1] == "mod":
-                result = data[0] % data[2]
-            elif data[1] == "div":
-                result = data[0] // data[2]
-            elif data[1] == "pow":
-                result = m.pow(data[0], data[2])
-            elif data[1] == "log_by":
-                result = m.log(data[0], data[2])
+            return "Unknown operation"
+
         return result
 
     def evaluate(self, expression):
@@ -112,3 +91,4 @@ if __name__ == "__main__":
     assert calculator.evaluate("sqrt 4") == 2
     assert calculator.evaluate("sqrt 4a") == "Input operands is not numbers"
     assert calculator.evaluate("sin 30") == -0.9880316240928618
+    assert calculator.evaluate("sqroot 49") == "Unknown operation"
